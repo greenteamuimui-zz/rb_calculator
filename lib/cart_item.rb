086967a701e1ff0f865736a76a1115b4580b6487
greenteamuimui <siuding0601@gmail.com>
2017-09-16 19:03:06 -0700

    Add CartItem and System classes

---
+++ lib/cart_item.rb
@@ -0,0 +1,4 @@
+class CartItem
+  def initialize(item_description_hash)
+  end
+end
---
+++ lib/system.rb
@@ -0,0 +1,9 @@
+class System
+  def run(cartlist_json_location, catalog_json_location)
+    
+  end
+
+  def display_price
+
+  end
+end
